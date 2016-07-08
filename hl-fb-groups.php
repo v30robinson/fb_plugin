3e1b6a0817d332a1275b9b5ca6631ba229adc6a3
Stanislav Vysotskyi <stanislav.vysotskyi@mev.com>
2016-07-08 14:37:02 +0300

    Fixed install problem, when you using 2 the same function in the bootstrap file.

--- hl-fb-groups.php
+++ hl-fb-groups.php
@@ -28,113 +28,118 @@
  *
  */
 
-// If this file is called directly, abort.
-if (!defined('WPINC')) {
-    die;
-}
-
-function isPluginInstalled()
+class HLGroups
 {
-    return file_exists(plugin_dir_path( __FILE__ ) . 'vendor/autoload.php');
-}
 
-/**
- * Check plugin dependency
- * @return bool
- */
-function checkPluginDependency()
-{
-    if (!is_plugin_active('wp-facebook-login/facebook-login.php')) {
-        echo '<b>Dependency error!</b> 
+    /**
+     * Check, is plugin installed
+     * @return bool
+     */
+    private function isInstalled()
+    {
+        return file_exists(plugin_dir_path( __FILE__ ) . 'vendor/autoload.php');
+    }
+
+    /**
+     * Check plugin dependency
+     * @return bool
+     */
+    private function checkPluginDependency()
+    {
+        if (!is_plugin_active('wp-facebook-login/facebook-login.php')) {
+            echo '<b>Dependency error!</b> 
               For plugin work need to install and activate 
               <a href="https://wordpress.org/plugins/wp-facebook-login/" target="_blank">Facebook Login</a> plugin!
              ';
-        return false;
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-/**
- * Check composer dependency
- * @return bool
- */
-function checkComposerDependency()
-{
-    if (!defined('HL_COMPOSER')) {
-        echo '<b>Composer error!</b> 
+    /**
+     * Check composer dependency
+     * @return bool
+     */
+    private  function checkComposerDependency()
+    {
+        if (!defined('HL_COMPOSER')) {
+            echo '<b>Composer error!</b> 
               You need to add <b>HL_COMPOSER</b> const to the WordPress config with path to composer file. It\'s needed 
               for installing plugin dependency (twig and etc.)
               ';
-        return false;
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-/**
- * The code that runs during plugin activation.
- */
-function activate_hl_fb_groups()
-{
-    if (checkPluginDependency() && checkComposerDependency()) {
-        putenv("COMPOSER_HOME=" . HL_COMPOSER . '.composer');
-        exec('(cd ' . plugin_dir_path(__FILE__) .' && php ' . HL_COMPOSER . 'composer install) 2>&1');
-        return;
+    /**
+     * The code that runs during plugin activation.
+     */
+    private function activate_hl_fb_groups()
+    {
+        if ($this->checkPluginDependency() && $this->checkComposerDependency()) {
+            putenv("COMPOSER_HOME=" . HL_COMPOSER . '.composer');
+            exec('(cd ' . plugin_dir_path(__FILE__) .' && php ' . HL_COMPOSER . 'composer install) 2>&1');
+            return;
+        }
+        exit();
     }
-    exit();
-}
 
-/**
- * The code that runs during plugin deactivation.
- */
-function deactivate_hl_fb_groups()
-{
-    exec('rm -rfv ' . plugin_dir_path(__FILE__) . 'vendor/*');
-}
+    /**
+     * The code that runs during plugin deactivation.
+     */
+    private function deactivate_hl_fb_groups()
+    {
+        exec('rm -rfv ' . plugin_dir_path(__FILE__) . 'vendor/*');
+    }
 
-/**
- * The core plugin classes that is used to define internationalization, admin-specific hooks,
- * and public-facing site hooks.
- */
-function includeClasses()
-{
-    require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/class-hl-fb-groups-config.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/class-hl-fb-groups-core.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-entity-manager.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-facebook-manager.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-local-manager.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-from.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-request.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-template.php';
-    require_once plugin_dir_path( __FILE__ ) . 'admin/class-hl-fb-groups-admin.php';
-    require_once plugin_dir_path( __FILE__ ) . 'public/class-hl-fb-groups-public.php';
-}
+    /**
+     * The core plugin classes that is used to define internationalization, admin-specific hooks,
+     * and public-facing site hooks.
+     */
+    private function includeClasses()
+    {
+        require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/class-hl-fb-groups-config.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/class-hl-fb-groups-core.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-entity-manager.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-facebook-manager.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-local-manager.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-from.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-request.php';
+        require_once plugin_dir_path( __FILE__ ) . 'includes/modules/class-hl-groups-template.php';
+        require_once plugin_dir_path( __FILE__ ) . 'admin/class-hl-fb-groups-admin.php';
+        require_once plugin_dir_path( __FILE__ ) . 'public/class-hl-fb-groups-public.php';
+    }
 
-/**
- * Init core lib and setup activate/deactivate setting
- */
-function includeCore()
-{
-    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    /**
+     * Init core lib and setup activate/deactivate setting
+     */
+    public function initCore()
+    {
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
-    register_activation_hook( __FILE__, 'activate_hl_fb_groups');
-    register_deactivation_hook( __FILE__, 'deactivate_hl_fb_groups');
-}
+        register_activation_hook( __FILE__, [$this, 'activate_hl_fb_groups']);
+        register_deactivation_hook( __FILE__, [$this, 'deactivate_hl_fb_groups']);
+    }
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- */
-function runPlugin()
-{
-    if (isPluginInstalled() && is_plugin_active('wp-facebook-login/facebook-login.php')) {
-        includeClasses();
-        $plugin = !is_admin() ? new HLGroupsPublic() : new HLGroupsAdmin();
+    /**
+     * Begins execution of the plugin.
+     * Since everything within the plugin is registered via hooks,
+     * then kicking off the plugin from this point in the file does
+     * not affect the page life cycle.
+     */
+    public function run()
+    {
+        if ($this->isInstalled() && is_plugin_active('wp-facebook-login/facebook-login.php')) {
+            $this->includeClasses();
+            $plugin = !is_admin() ? new HLGroupsPublic() : new HLGroupsAdmin();
+        }
     }
 }
 
-includeCore();
-runPlugin();
+if (defined('WPINC')) {
+    $plugin = new HLGroups();
+    $plugin->initCore();
+    $plugin->run();
+}
